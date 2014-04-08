db4abb62d3ab4dbb37ef5bd78f855bd16d1eb393
Resets <resets@free.fr>
2014-04-08 21:14:59 +0200

    Modifications player et sprites
    Ecriture du moteur de gravité newton.
    Simplification  du module sprite

--- lib/player.py
+++ lib/player.py
@@ -6,5 +6,30 @@ from pygame.locals import *
 #####################################
 
 class Player():
-	def __init__(self):
-		vitesse = 
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(image).convert_alpha()
+        self.rect = self.image.get_rect()
+
+    def gravite(self):
+        """Actualise la position du personnage
+        lors d'un saut en fonction de la gravité"""
+        g = 9.81
+        pi = 3.14
+
+        t = 0
+
+        v_init = vitesse_x
+        angle_init = pi/3
+
+        v_x = cos(angle_init)*v_init
+        v_y = sin(angle_init)*v_init
+
+        position_relative_x = (int)(v_x*t);
+        position_relative_y = (int)((v_y*t)-((g*t*t)/2000))
+
+        self.image.rect.x = self.image.rect.x + position_relative_x
+        self.image.rect.y = self.image.rect.y - position_relative_y
+
+        t += 10
+
--- lib/sprites.py
+++ lib/sprites.py
@@ -5,23 +5,8 @@ from pygame.locals import *
 #--Fonctions de création de sprite--#
 #####################################
 
-class Plateformes(pygame.sprite.Sprite):
+class Bloc(pygame.sprite.Sprite):
     """Création d'un sprite plateforme"""
-
-    def __init__(self, image, x, y):
-        """Construction"""
-        pygame.sprite.Sprite.__init__(self)
-
-        self.image = pygame.image.load(image).convert_alpha()
-        self.rect = self.image.get_rect()
-
-        self.rect.x = x
-        self.rect.y = y
-        
-        self.platform_list.add(self)
-
-class Ennemis(pygame.sprite.Sprite):
-    """Création d'un sprite ennemie"""
 
     def __init__(self, image):
         """Construction"""
@@ -29,8 +14,3 @@ class Ennemis(pygame.sprite.Sprite):
 
         self.image = pygame.image.load(image).convert_alpha()
         self.rect = self.image.get_rect()
-
-        self.rect.x = x
-        self.rect.y = y
-        
-        self.platform_list.add(self)
