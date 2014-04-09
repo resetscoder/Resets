813628ac4aa1288d3810e225ab492a036f1f918d
Resets <resets@free.fr>
2014-04-09 17:39:49 +0200

    Simplification , correction d'erreurs

--- lib/tiles.py
+++ lib/tiles.py
@@ -5,11 +5,4 @@ from pygame.locals import *
 #--Dictionnaire d'affiliation Couleur / Sprite--#
 #################################################
 
-affiliation_tile = {
-
-#niveau 1 - code binaire
-(0,0,0,0): None #arriere plan
-(51,204,51,0): 'image1_0'
-
-#niveau 2 - MS-DOS
-}
+affiliation_tile = {'(51,204,51,0)': '0','(1,1,1,1)': '1'}
