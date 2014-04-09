10be5bc3581cbe41293a2b4879711ce9f7fb8cab
Resets <resets@free.fr>
2014-04-09 17:40:51 +0200

    Modifications diverses
    - attribution a une variable d'une valeur de clé du dictionnaire tile
    - recherche dans le dictionnaire dans levelmap

--- lib/levelmap.py
+++ lib/levelmap.py
@@ -2,7 +2,7 @@ import pygame
 from pygame.locals import *
 
 import sprites
-import main
+import tiles
 
 #######################################
 #---Fonctions de creation de niveau---#
@@ -10,14 +10,14 @@ import main
 
 def RGBlevelmap(num_niveau):
     #penser à l'arrière plan !
-    del bloc_liste[:], tous_sprites_liste[:]    												#effacer les anciens sprites
-    img_niveau = pygame.image.load('Level_%s.png'%(num_niveau)).convert_alpha()		#recuper le tile
+    #del bloc_liste[:], tous_sprites_liste[:]    												#effacer les anciens sprites
+    img_niveau = pygame.image.load('Level_%s.png'%(num_niveau))		#recuper le tile
     for x in range(img_niveau.get_width()):
         for y in range(img_niveau.get_height()):									#parcours du tile pixel par pixel
             color = img_niveau.get_at((x,y))										#recuperation de la couleur
             x = x*(largeur/img_niveau.get_width())
             y = y*(hauteur/img_niveau.get_heigh())
-            if color == (51,204,51,0):
-                Bloc('image1_0.png')
-                Bloc.dessiner(x,y)
+            tile = affiliation_tile[str(color)]
+            image = 'image1_' + tile + '.png'
+            Bloc(image,x,y)
             #etc...
--- lib/sprites.py
+++ lib/sprites.py
@@ -8,17 +8,16 @@ from pygame.locals import *
 class Bloc(pygame.sprite.Sprite):
     """CrÃ©ation d'un sprite plateforme"""
 
-    def __init__(self, image):
+    def __init__(self, image, x, y):
         """Construction"""
         pygame.sprite.Sprite.__init__(self)
 
         self.image = pygame.image.load(image).convert_alpha()
         self.rect = self.image.get_rect()
 
-    def dessiner(self,x,y):
-
         self.rect.x = x
         self.rect.y = y
 
         self.bloc_liste.add(self)
         self.tous_sprites_liste.add(self)
+        
