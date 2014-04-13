bc0f3c26591bf1d65e194508fcadd70d74597a6a
Resets <resets@free.fr>
2014-04-13 16:13:58 +0200

    Modifications diverses

--- lib/levelmap.py
+++ lib/levelmap.py
@@ -27,19 +27,27 @@ class RGBlevelmap():
 
     def bloc_level(self,LARGEUR,HAUTEUR,bloc_liste,tous_sprites_liste):
         """Parcours le tile et charge les sprites"""
-        
+
+        #del bloc_liste[:], tous_sprites_liste[:]
+        #effacer les anciens sprites
+
         width = self.img_niveau.get_width()
         height = self.img_niveau.get_height()
+        
         for x in range(width):
             for y in range(height):                        #parcours du tile pixel par pixel
                 color = self.img_niveau.get_at((x,y))										#recuperation de la couleur
                 X = x*(1300/width)
                 Y = y*(560/height)
+                
                 if not color == (0,0,0,255):
                     tile = self.affiliation_tile[str(color)]
+
                     image = 'image1_' + tile + '.png'
                     bloc = sprites.Bloc(os.path.join(DOSSIER_DATA,image),X,Y,width,height)
+
                     bloc_liste.add(bloc)
                     tous_sprites_liste.add(bloc)
+                    
                 #etc...
         
